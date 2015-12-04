3a303c2d216ebe0cb294c70aee569864a4bc413f
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-12-03 19:37:59 -0500

    Add gzip compression to server responses.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
+var compression = require('compression');
 
 var app = express();
 
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(morgan('dev'));
+app.use(compression());
 
 //--------------------------------------------------------
 // Routing:
