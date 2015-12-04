6c2e0e8656066db13f66cdd5d0d114fa307c8f92
Justin Zimmerman <zimmy130@gmail.com>
2015-12-03 19:13:27 -0500

    modify gallery

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
-var compression = require('compression');
 
 var app = express();
 
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(morgan('dev'));
-app.use(compression());
 
 //--------------------------------------------------------
 // Routing:
